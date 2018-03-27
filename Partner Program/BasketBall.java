8a320c97613763999bc0cede9270506b4bb27abe
cpham1986 <35931875+cpham1986@users.noreply.github.com>
2018-03-27 13:12:34 -0400

    asd
    asd

---
+++ Partner Program/BasketBall.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class BasketBall here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BasketBall
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class BasketBall
+     */
+    public BasketBall()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
--- Partner Program/Sports.java
+++ Partner Program/Sports.java
@@ -7,27 +7,16 @@
  */
 public class Sports
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    String n, s;
 
     /**
      * Constructor for objects of class Sports
      */
-    public Sports()
+    public Sports(String name, String sport)
     {
-        // initialise instance variables
-        x = 0;
+        n = name;
+        s = sport.toLowerCase();
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
+    
 }
