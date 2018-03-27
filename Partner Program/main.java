37a2b4c7eb78f298a6b8343a561638c4babd0c44
nroy6967 <37844414+nroy6967@users.noreply.github.com>
2018-03-27 13:03:35 -0400

    thsgbs
    asdagsef

--- Partner Program/main.java
+++ Partner Program/main.java
@@ -5,7 +5,14 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.*;
 public class main
 {
-    public 
+    public static void main(){
+        Scanner input=new Scanner(System.in);
+        String name, sport;
+        System.out.println("Enter the name of the athlete: ");
+        name=input.nextLine();
+        
+    }
 }
