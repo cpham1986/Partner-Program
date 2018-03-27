26b9814f20a1162ead7b537ed8c9b06a9eceea9c
nroy6967 <37844414+nroy6967@users.noreply.github.com>
2018-03-27 13:12:18 -0400

    yes

--- Partner Program/main.java
+++ Partner Program/main.java
@@ -9,10 +9,11 @@ import java.util.*;
 public class main
 {
     public static void main(){
-                Scanner input=new Scanner(System.in);
+        Scanner input=new Scanner(System.in);
         String name, sport;
         System.out.println("Enter the name of the athlete: ");
         name=input.nextLine();
-        
+        System.out.println("Enter sport: ");
+        sport=input.nextLine();
     }
 }
