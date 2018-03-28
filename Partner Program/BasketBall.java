af3ca688ea20add4eb482d25c7c2029af661196d
nroy6967 <37844414+nroy6967@users.noreply.github.com>
2018-03-28 12:48:03 -0400

    afda

--- Partner Program/BasketBall.java
+++ Partner Program/BasketBall.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class BasketBall
+public class BasketBall extends Sports
 {
     // instance variables - replace the example below with your own
     private int x;
@@ -17,17 +17,5 @@ public class BasketBall
     {
         // initialise instance variables
         x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
     }
 }
--- Partner Program/Football.java
+++ Partner Program/Football.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Football
+public class Football extends Sports
 {
     // instance variables - replace the example below with your own
     private int x;
@@ -19,15 +19,5 @@ public class Football
         x = 0;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
