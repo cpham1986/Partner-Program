a3f917a74dd93b7cd6061fa97dc154c172fd3ef0
cpham1986 <35931875+cpham1986@users.noreply.github.com>
2018-03-28 12:36:34 -0400

    asd
    asd

--- Partner Program/Hockey.java
+++ Partner Program/Hockey.java
@@ -15,18 +15,8 @@ public class Hockey extends Sports
      */
     public Hockey()
     {
-        super(n);
+        
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
