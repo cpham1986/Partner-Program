b955d8e9e29b88d0c44fd4009c35258eaad4c8e9
nroy6967 <37844414+nroy6967@users.noreply.github.com>
2018-03-28 13:04:29 -0400

    fa

--- Partner Program/main.java
+++ Partner Program/main.java
@@ -9,30 +9,34 @@ import java.util.*;
 public class main
 {
     public static void main(){
+        int start, tgames, sgames;
         Scanner input = new Scanner(System.in);
         String name, sport;
+        Sports player;
         System.out.println("Enter the name of the athlete: ");
         name=input.nextLine();
-<<<<<<< HEAD
+
         System.out.println("Enter sport: ");
         sport=input.nextLine();
-        
-=======
+
         do{
             System.out.println("Enter sport: ");
             sport=input.nextLine();
         } while(sport.equalsIgnoreCase("hockey")!=false||sport.equalsIgnoreCase("football")!=false||sport.equalsIgnoreCase("basketball")!=false);
         
-        
-        
-        
-        
-        
         switch(sport){
-            case "hockey":;break;
-            case "football":;break;
-            case "basketball":;break;
+            case "hockey": player= new Hockey(name, sport);break;
+            case "football": player = new Football(name, sport);break;
+            case "basketball":player = new BasketBall(name, sport);break;
         }
->>>>>>> a3bc749d235e119d760531f17ca6b01c8781ce77
+        System.out.println("Enter number of games started: ");
+        start=input.nextInt();
+        System.out.println("Entered total number of games played int: ");
+        tgames=input.nextInt();
+        System.out.println("Enter the total games in the season: ");
+        sgames=input.nextInt();
+        
+
+
     }
 }
